6f20e66b6573438fb37af70f0ee61ad9a4db8cd9
Thiago Gualberto <thiago.gou@gmail.com>
2021-12-07 21:32:03 -0300

    MongoDB connection file.

---
+++ src/database/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const MONGO_DB_URL = 'mongodb://localhost:27017/testtrybe';
+// const DB_NAME = 'Cookmaster';
+
+// Para o avaliador funcionar alterar a conexão para:
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+// const DB_NAME = 'Cookmaster';
+
+async function main() {
+    await mongoose.connect(MONGO_DB_URL, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    });
+}
+
+main().then(() => {
+    console.log('Connect to MongoDB.');
+}).catch((err) => {
+    console.log(err);
+});
