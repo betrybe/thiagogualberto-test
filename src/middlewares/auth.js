f5e28c908b9a11f9d5af6cf4300a1597cba3e665
Thiago Gualberto <thiago.gou@gmail.com>
2021-12-08 20:45:15 -0300

    Improved authentication middleware.

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,11 +5,16 @@ const authConfig = require('../config/config');
 module.exports = {
     async validateToken(req, res, next) {
         const token = req.headers.authorization;
+
+        if (!token) {
+            return res.status(401).json({ message: 'missing auth token' });
+        }
         
         try {
             const decode = await promisify(jwt.verify)(token, authConfig.jwt.secret);
             
             req.id = decode.id;
+            req.role = decode.role;
 
             return next();
         } catch (err) { // Invalid Token
